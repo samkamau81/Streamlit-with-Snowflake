8fe451257fd053bc096c22317f154368ba68e008
Samuel Waweru <samuelkamau81@yahoo.com>
2023-05-04 19:57:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ session = create_session()
 st.success("Connected to Snowflake!")
 
 # Load data table
-@st.cache_data
+@st.cache_data(max_entries=1000)
 def load_data(table_name):
     ## Read in data table
     table = session.table(table_name)
